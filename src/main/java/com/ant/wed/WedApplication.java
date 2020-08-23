09ac5b2e7411044fa411b7911dfdeb217a2332cc
yodatk <37527725+yodatk@users.noreply.github.com>
2020-08-23 17:45:39 +0300

    initial commit: 1. client side - initial setup for react including typescript,bootstrap,mdboostrap react-router-dom, axio, and expresss. 2. server side - maven+spring with mongodb,springweb,and REST.

---
+++ src/main/java/com/ant/wed/WedApplication.java
@@ -0,0 +1,14 @@
+package com.ant.wed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedApplication {
+
+	public static void main(String[] args) {
+		System.out.println("habibi supreme");
+		SpringApplication.run(WedApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/client/src/App.tsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import './App.css';
+
+import HomePage from "./pages/HomePage";
+
+class App extends Component {
+  render(){
+    return (
+
+        <Router>
+            {/*home-page*/}
+          <Route exact path="/">
+            <HomePage/>
+          </Route>
+        </Router>
+    );
+  }
+
+}
+
+export default App;
